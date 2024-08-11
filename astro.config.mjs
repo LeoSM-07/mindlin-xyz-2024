5b176b4c341a06d28f9419dc128d4f3967317b38
LeoSM_07 <69401930+LeoSM-07@users.noreply.github.com>
2024-08-11 13:43:37 -0700

    Various fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
 import mdx from "@astrojs/mdx";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  output: "static"
+  output: "static",
+  rehypePlugins: [rehypeAutolinkHeadings],
 });
